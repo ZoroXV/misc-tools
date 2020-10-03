3995ff97f407ff5d8a7558929b8448524e8580dd
Victor Le Corre <victor.le-corre@epita.fr>
2020-10-03 20:17:44 +0200

    Auto-git: add first version of auto-git.sh

---
+++ auto-git.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo -e "\n"
+echo -e "Starting \e[1m\e[32mAuto Git...\e[0m\n"
+
+#git add part
+echo -e "Wich files you want to add:\n"
+read files_staged
+git add $files_staged
+
+git status
+
+#auto clang-format
+#clang-format -i $files_staged
+
+#git commit part
+git commit
+
+#git tag part
+echo -e "Give a \e[1m\e[95mtag name:\e[0m\n"
+read tag_name
+
+echo -e "Give a \e[1m\e[95mtag message:\e[0m\n"
+read tag_msg
+
+git tag -a "$tag_name" -m "$tag_msg"
+
+#git push part
+echo -e "Commit will be push...\n"
+git push --follow-tags
+
+#end
+echo -e "\e[1m\e[93mAuto Git leave the game.\e[0m\n"
